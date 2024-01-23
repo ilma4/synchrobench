3b9f6211ce4360fb0344e3e9eadacb10fd4743f5
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-01-23 20:51:44 +0100

    hand over hand algorithm. test script

---
+++ java/src/linkedlists/lockbased/HandOverHandListIntSet.java
@@ -0,0 +1,162 @@
+package linkedlists.lockbased;
+
+import contention.abstractions.AbstractCompositionalIntSet;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class HandOverHandListIntSet extends AbstractCompositionalIntSet {
+
+    // sentinel nodes
+    private Node head;
+    private Node tail;
+
+    public HandOverHandListIntSet() {
+        head = new Node(Integer.MIN_VALUE);
+        tail = new Node(Integer.MAX_VALUE);
+        head.next = tail;
+    }
+
+    @Override
+    public boolean addInt(int item) {
+        Node pred = head;
+        final int key = item;
+        pred.lock();
+        try {
+            Node curr = pred.next;
+            curr.lock();
+            try {
+                while (curr.key < key) {
+                    pred.unlock();
+                    pred = curr;
+                    curr = curr.next;
+                    curr.lock();
+                }
+                if (curr.key == key) {
+                    return false;
+                }
+                Node node = new Node(item);
+                node.next = curr;
+                pred.next = node;
+                return true;
+            } finally {
+                curr.unlock();
+            }
+        } finally {
+            pred.unlock();
+        }
+    }
+
+    @Override
+    public boolean removeInt(int item) {
+        Node pred = head;
+        final int key = item;
+        pred.lock();
+        try {
+            Node curr = pred.next;
+            curr.lock();
+            try {
+                while (curr.key < key) {
+                    pred.unlock();
+                    pred = curr;
+                    curr = curr.next;
+                    curr.lock();
+                }
+                if (curr.key == key) {
+                    pred.next = curr.next;
+                    return true;
+                }
+                return false;
+            } finally {
+                curr.unlock();
+            }
+        } finally {
+            pred.unlock();
+        }
+    }
+
+    @Override
+    public boolean containsInt(int item) {
+        final int key = item;
+        Node pred = head;
+        pred.lock();
+        try {
+            Node curr = pred.next;
+            curr.lock();
+            try {
+                while (curr.key < key) {
+                    pred.unlock();
+                    pred = curr;
+                    curr = curr.next;
+                    curr.lock();
+                }
+                return curr.key == key;
+            } finally {
+                curr.unlock();
+            }
+        } finally {
+            pred.unlock();
+        }
+    }
+
+/*
+    @Override
+    public boolean containsInt(int item) {
+        Node curr = head;
+        curr.lock();
+        try {
+            while (curr.key < item) {
+                Node next = curr.next;
+                next.lock();
+                curr.unlock();
+                curr = next;
+            }
+            return curr.key == item;
+        } finally {
+            curr.unlock();
+        }
+    }
+*/
+
+    private static class Node {
+
+        public int key;
+        public Node next;
+        private final Lock lock;
+
+        Node(int key) {
+            this.key = key;
+            this.next = null;
+            this.lock = new ReentrantLock();
+        }
+
+        public void lock() {
+            lock.lock();
+        }
+
+        public void unlock() {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void clear() {
+        head = new Node(Integer.MIN_VALUE);
+        head.next = new Node(Integer.MAX_VALUE);
+    }
+
+    /**
+     * Non atomic and thread-unsafe
+     */
+    @Override
+    public int size() {
+        int count = 0;
+
+        Node curr = head.next;
+        while (curr.key != Integer.MAX_VALUE) {
+            curr = curr.next;
+            count++;
+        }
+        return count;
+    }
+}
