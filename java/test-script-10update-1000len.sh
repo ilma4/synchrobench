ec6c9892486743649b103d64db22f3e8ed88ce52
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-01-23 23:18:49 +0100

    script for third

---
+++ java/test-script-10update-1000len.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir upd10_size1000
+cd upd10_size1000 || echo "Cant create dir" && exit 1
+
+THREADS=12
+
+for i in 1 2 3
+do
+case $1 in
+"1" )	echo CoarseGrainedListIntSet
+    OUTPUT="CoarseGrainedListBasedSet"
+;;
+"2" )	echo HandOverHandListIntSet
+    OUTPUT="HandOverHandListIntSet"
+;;
+"3" )	echo LazyLinkedListSortedSet
+    OUTPUT="LazyLinkedListSortedSet"
+;;
+*)		echo "Specify algorithm"
+    exit 0
+esac
+echo "Who I am: $OUTPUT on `uname -n`"
+echo "started on" `date`
+	for j in 0 10
+	do
+		echo "→ $OUTPUT	$i	$j"
+		java -cp bin contention.benchmark.Test -b linkedlists.lockbased.$OUTPUT -d 2000 -t "$THREADS" -u $j -i 1000 -r 2000 -W 0 | grep Throughput > "$i.log"
+#		echo "→ $OUTPUT	$i	$j	without -W 0"
+#		java -cp bin contention.benchmark.Test -b linkedlists.lockbased.$OUTPUT -d 3000 -t $i -u $j -i 1024 -r 2048 | grep Throughput
+	done 
+	# wait
+done
+echo "finished on" `date`
+echo "DONE \o/"
+
