b6848db9a2a366778df13dd63fe6eb920b97abc5
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-01-24 00:04:29 +0100

    fix

--- java/test-script-10update-1000len.sh
+++ java/test-script-10update-1000len.sh
@@ -3,31 +3,17 @@
 DIR="upd10_size1000"
 mkdir -p "$DIR"
 
-THREADS=12
 
-for i in 1 2 3
+for alg in CoarseGrainedListIntSet  HandOverHandListIntSet  LazyLinkedListSortedSet
 do
-  case $i in
-  "1" )	echo CoarseGrainedListIntSet
-      OUTPUT="CoarseGrainedListBasedSet"
-  ;;
-  "2" )	echo HandOverHandListIntSet
-      OUTPUT="HandOverHandListIntSet"
-  ;;
-  "3" )	echo LazyLinkedListSortedSet
-      OUTPUT="LazyLinkedListSortedSet"
-  ;;
-  *)		echo "Specify algorithm"
-      exit 0
-  esac
-  echo "Who I am: $OUTPUT on `uname -n`"
+  echo "Who I am: $alg on `uname -n`"
   echo "started on" `date`
-  rm "$DIR/$i.log"
-	for j in `seq 10`
+  rm "$DIR/$alg.log"
+	for tr in $(seq 1 16)
 	do
-		java -cp bin contention.benchmark.Test -b linkedlists.lockbased.$OUTPUT -d 2000 -t "$THREADS" -u 10 -i 1000 -r 2000 -W 0 | grep Throughput >> "$DIR/$i.log"
-#		echo "→ $OUTPUT	$i	$j	without -W 0"
-#		java -cp bin contention.benchmark.Test -b linkedlists.lockbased.$OUTPUT -d 3000 -t $i -u $j -i 1024 -r 2048 | grep Throughput
+		java -cp bin contention.benchmark.Test -b linkedlists.lockbased.$alg -d 2000 -t "$tr" -u 10 -i 1000 -r 2000 -W 0 | grep Throughput >> "$DIR/$alg.log"
+#		echo "→ $OUTPUT	$alg	$tr	without -W 0"
+#		java -cp bin contention.benchmark.Test -b linkedlists.lockbased.$OUTPUT -d 3000 -t $alg -u $tr -alg 1024 -r 2048 | grep Throughput
 	done 
 	# wait
 done
